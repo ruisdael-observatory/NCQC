5e1421ba24b5acf1bef47ce0203d7ead7bba6df5
Jesse Vleeschdraager <J.C.Vleeschdraager@student.tudelft.nl>
2024-05-28 22:55:12 +0200

    update test pipeline + fix pylint

--- netcdfqc/main.py
+++ netcdfqc/main.py
@@ -3,6 +3,10 @@ Main.py
 '''
 
 def get_version() -> str:
+    '''
+    Returns a string with the current version.
+    Manually updated, just a test function.
+    '''
     return "0.1.0"
 
 if __name__ == '__main__':
