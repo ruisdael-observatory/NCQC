86225a96c3267dc92811e267b4a4549db98d8436
Vasil <v.chirov@student.tudelft.nl>
2024-06-05 15:27:16 +0200

    added tests

--- tests/test_QualityControl.py
+++ tests/test_QualityControl.py
@@ -1,7 +1,8 @@
 import unittest
+from pathlib import Path
 from unittest.mock import patch
 
-from netcdfqc.QCnetCDF import QualityControl
+from netcdfqc.QCnetCDF import QualityControl, yaml2dict
 
 
 class TestQualityControl(unittest.TestCase):
@@ -9,81 +10,174 @@ class TestQualityControl(unittest.TestCase):
     @patch('netcdfqc.QCnetCDF.yaml2dict')
     def test_add_qc_checks_conf(self, mock_yaml2dict):
         qc_obj = QualityControl()
-        qc_obj.qc_checks = {
-            'v1': {
-                'l': 1,
-                'r': 2
+        qc_obj.qc_checks_dims = {
+            'example_dimension': {'existence': True}
+        }
+        qc_obj.qc_checks_vars = {
+            'example_variable': {
+                'existence': True,
+                'boundaries': {'do': True, 'lower': 1, 'upper': 2}
             }
         }
+        qc_obj.qc_checks_gl_attrs = {'existence': True, 'emptiness': True}
 
         def my_side_effect(arg):
-            if arg == 'path':
+            if arg == Path('path'):
                 new_checks_dict = {
-                    'v2': {
-                        'l': 2,
-                        'r': 3
+                    'dimensions': {
+                        'example_dimension_2': {'existence': False}
+                    },
+                    'variables': {
+                        'example_variable_2': {
+                            'existence': False,
+                            'boundaries': {'do': True, 'lower': 3, 'upper': 4}
+                        }
+                    },
+                    'global attributes': {
+                        'existence': True, 'emptiness': True
                     }
                 }
                 return new_checks_dict
-            else:
-                return {}
+            return {}
 
         mock_yaml2dict.side_effect = my_side_effect
 
         expected_result = {
-            'v1': {
-                'l': 1,
-                'r': 2
+            'dimensions': {
+                'example_dimension': {'existence': True},
+                'example_dimension_2': {'existence': False}
             },
-            'v2': {
-                'l': 2,
-                'r': 3
+            'variables': {
+                'example_variable': {
+                    'existence': True,
+                    'boundaries': {'do': True, 'lower': 1, 'upper': 2}
+                },
+                'example_variable_2': {
+                    'existence': False,
+                    'boundaries': {'do': True, 'lower': 3, 'upper': 4}
+                }
+            },
+            'global attributes': {
+                'existence': True, 'emptiness': True
             }
         }
 
-        qc_obj.add_qc_checks_conf('path')
-        assert qc_obj.qc_checks == expected_result
-
-        qc_obj.qc_checks = {
-            'v1': {
-                'l': 1,
-                'r': 2
-            }
-        }
-        qc_obj.add_qc_checks_conf('wrong_path')
-        assert qc_obj.qc_checks == {
-            'v1': {
-                'l': 1,
-                'r': 2
-            }
-        }
+        qc_obj.add_qc_checks_conf(Path('path'))
+        assert qc_obj.qc_checks_dims == expected_result['dimensions']
+        assert qc_obj.qc_checks_vars == expected_result['variables']
+        assert qc_obj.qc_checks_gl_attrs == expected_result['global attributes']
 
     def test_add_qc_checks_dict(self):
         qc_obj = QualityControl()
-        qc_obj.qc_checks = {
-            'v1': {
-                'l': 1,
-                'r': 2
+        qc_obj.qc_checks_dims = {
+            'example_dimension': {'existence': True}
+        }
+        qc_obj.qc_checks_vars = {
+            'example_variable': {
+                'existence': True,
+                'boundaries': {'do': True, 'lower': 1, 'upper': 2}
             }
         }
+        qc_obj.qc_checks_gl_attrs = {'existence': True, 'emptiness': True}
+
         new_checks = {
-            'v2': {
-                'l': 2,
-                'r': 3
+            'dimensions': {
+                'example_dimension_2': {'existence': False}
+            },
+            'variables': {
+                'example_variable_2': {
+                    'existence': False,
+                    'boundaries': {'do': True, 'lower': 3, 'upper': 4}
+                }
+            },
+            'global attributes': {
+                'existence': True, 'emptiness': True
             }
         }
 
         expected_result = {
-            'v1': {
-                'l': 1,
-                'r': 2
+            'dimensions': {
+                'example_dimension': {'existence': True},
+                'example_dimension_2': {'existence': False}
             },
-            'v2': {
-                'l': 2,
-                'r': 3
+            'variables': {
+                'example_variable': {
+                    'existence': True,
+                    'boundaries': {'do': True, 'lower': 1, 'upper': 2}
+                },
+                'example_variable_2': {
+                    'existence': False,
+                    'boundaries': {'do': True, 'lower': 3, 'upper': 4}
+                }
+            },
+            'global attributes': {
+                'existence': True, 'emptiness': True
             }
         }
 
         qc_obj.add_qc_checks_dict(new_checks)
 
-        assert qc_obj.qc_checks == expected_result
+        assert qc_obj.qc_checks_dims == expected_result['dimensions']
+        assert qc_obj.qc_checks_vars == expected_result['variables']
+        assert qc_obj.qc_checks_gl_attrs == expected_result['global attributes']
+
+    @patch('netcdfqc.QCnetCDF.yaml2dict')
+    def test_replace_qc_checks_conf(self, mock_yaml2dict):
+        qc_obj = QualityControl()
+        qc_obj.qc_checks_dims = {
+            'example_dimension': {'existence': True}
+        }
+        qc_obj.qc_checks_vars = {
+            'example_variable': {
+                'existence': True,
+                'boundaries': {'do': True, 'lower': 1, 'upper': 2}
+            }
+        }
+        qc_obj.qc_checks_gl_attrs = {'existence': True, 'emptiness': True}
+
+        new_checks_dict = {
+            'dimensions': {
+                'example_dimension_2': {'existence': False}
+            },
+            'variables': {
+                'example_variable_2': {
+                    'existence': False,
+                    'boundaries': {'do': True, 'lower': 3, 'upper': 4}
+                }
+            },
+            'global attributes': {
+                'existence': True, 'emptiness': True
+            }
+        }
+
+        def my_side_effect(arg):
+            if arg == Path('path'):
+                return new_checks_dict
+            return {}
+
+        mock_yaml2dict.side_effect = my_side_effect
+
+        qc_obj.replace_qc_checks_conf(Path('path'))
+        assert qc_obj.qc_checks_dims == new_checks_dict['dimensions']
+        assert qc_obj.qc_checks_vars == new_checks_dict['variables']
+        assert qc_obj.qc_checks_gl_attrs == new_checks_dict['global attributes']
+
+    def test_yaml2dict(self):
+        res = yaml2dict(Path('../example_config.yaml'))
+        assert res == {
+            'dimensions': {'example_dimension': {'does_it_exist': True}},
+            'variables': {
+                'example_variable': {
+                    'does_it_exist_check': True,
+                    'is_data_within_boundaries_check': {
+                        'perform_check': True,
+                        'lower_bound': 0,
+                        'upper_bound': 1
+                    }
+                }
+            },
+            'global attributes': {'example_gl_attr': {
+                'does_it_exist_check': True,
+                'is_it_empty_check': True
+            }}
+        }
