19d525c38e4166575f14ab4c502d1855355eb367
Vasil <v.chirov@student.tudelft.nl>
2024-06-05 15:43:31 +0200

    added documentation

--- netcdfqc/QCnetCDF.py
+++ netcdfqc/QCnetCDF.py
@@ -1,42 +1,99 @@
+"""
+Module dedicated to the main logic of the netCDF quality control library
+"""
 from pathlib import Path
 
-import netCDF4
+import netCDF4  # pylint: disable=unused-import
 import yaml
+
+from netcdfqc.log import LoggerQC
 
 
 class QualityControl:
+    """
+    Class dedicated to reading desired checks from config files
+    and performing the quality control checks to netCDF files
+
+    Attributes:
+    - qc_checks_dims: checks for the dimensions of a netCDF file
+    - qc_checks_vars: checks for the variables (and data) of a netCDF file
+    - qc_checks_gl_attr: checks for the global attributes of a netCDF file
+    - logger: logger for errors, warnings, info, and creation of reports
+
+    Methods:
+    - add_qc_checks_conf: add checks via a config file
+    - add_qc_checks_dict: add checks via a dictionary
+    - replace_qc_checks_conf: replace checks via a config file
+    """
     def __init__(self):
-        self.qc_checks: dict = {}
+        """
+        Constructor for the QualityControl objects
+        """
+        self.qc_checks_dims: dict = {}
+        self.qc_checks_vars: dict = {}
+        self.qc_checks_gl_attrs: dict = {}
+        self.logger = LoggerQC()
 
     def add_qc_checks_conf(self, path_qc_checks_file: Path):
+        """
+        Method dedicated to adding quality control checks via a provided config file
+        :param path_qc_checks_file: path to the config file
+        """
         new_checks_dict = yaml2dict(path_qc_checks_file)
-        self.qc_checks.update(new_checks_dict)
+        self.add_qc_checks_dict(dict_qc_checks=new_checks_dict)
 
     def add_qc_checks_dict(self, dict_qc_checks: dict):
-        self.qc_checks.update(dict_qc_checks)
+        """
+        Method dedicated to adding quality control checks th
+        :param dict_qc_checks: the dictionary containing the checks
+        """
+        if 'dimensions' not in list(dict_qc_checks.keys()):
+            self.logger.add_error(error="missing dimensions checks in provided config_file/dict")
+        if 'variables' not in list(dict_qc_checks.keys()):
+            self.logger.add_error(error="missing variables checks in provided config_file/dict")
+        if 'global attributes' not in list(dict_qc_checks.keys()):
+            self.logger.add_error(error="missing global attributes checks in provided config_file/dict")
+        new_checks_dims_dict = dict_qc_checks['dimensions']
+        new_checks_vars_dict = dict_qc_checks['variables']
+        new_checks_gl_attrs_dict = dict_qc_checks['global attributes']
+        self.qc_checks_dims.update(new_checks_dims_dict)
+        self.qc_checks_vars.update(new_checks_vars_dict)
+        self.qc_checks_gl_attrs.update(new_checks_gl_attrs_dict)
 
-    def boundary_check(self, nc_file_path: Path):
-        nc_file_dict = netCDF4.Dataset(nc_file_path)
+    def replace_qc_checks_conf(self, path_qc_checks_file: Path):
+        """
+        Method dedicated to replacing the current checks with the ones from a config file
+        :param path_qc_checks_file: path to the config file
+        """
+        self.qc_checks_dims = {}
+        self.qc_checks_vars = {}
+        self.qc_checks_gl_attrs = {}
+        new_checks_dict = yaml2dict(path_qc_checks_file)
+        self.add_qc_checks_dict(new_checks_dict)
 
-        vars_to_check = list(self.qc_checks.keys())
-        vars_nc_file = list(nc_file_dict.variables.keys())
-
-        for var_name in vars_to_check:
-            if var_name not in vars_nc_file:
-                print(var_name, " not in netCDF file")
-                continue
-
-            lower_bound = self.qc_checks[var_name]['lower']
-            upper_bound = self.qc_checks[var_name]['upper']
-
-            var_values = nc_file_dict[var_name][:]
-
-            for val in var_values:
-                if val < lower_bound or val > upper_bound:
-                    print("fail - ", var_name, " out of bounds")
-                    return
-
-        print("success")
+    def boundary_check(self, nc_file_path: Path):  # pylint: disable=missing-function-docstring
+        # nc_file_dict = netCDF4.Dataset(nc_file_path)
+        #
+        # vars_to_check = list(self.qc_checks.keys())
+        # vars_nc_file = list(nc_file_dict.variables.keys())
+        #
+        # for var_name in vars_to_check:
+        #     if var_name not in vars_nc_file:
+        #         print(var_name, " not in netCDF file")
+        #         continue
+        #
+        #     lower_bound = self.qc_checks[var_name]['lower']
+        #     upper_bound = self.qc_checks[var_name]['upper']
+        #
+        #     var_values = nc_file_dict[var_name][:]
+        #
+        #     for val in var_values:
+        #         if val < lower_bound or val > upper_bound:
+        #             print("fail - ", var_name, " out of bounds")
+        #             return
+        #
+        # print("success")
+        pass
 
 
 def yaml2dict(path: Path) -> dict:
@@ -49,16 +106,3 @@ def yaml2dict(path: Path) -> dict:
         yaml_content = yaml_f.read()
         yaml_dict = yaml.safe_load(yaml_content)
     return yaml_dict
-
-
-if __name__ == '__main__':
-    test_dict = {
-        'velocity_spread': {
-            'lower': 0.08,
-            'upper': 3.3
-        }
-    }
-
-    qc = QualityControl()
-    qc.add_qc_checks_dict(test_dict)
-    qc.boundary_check('20240430_Green_Village-GV_PAR008.nc')
--- netcdfqc/log.py
+++ netcdfqc/log.py
@@ -1,23 +1,65 @@
+"""
+Module dedicated to the implementation of the logger for the netCDF quality control library
+"""
 from datetime import date, datetime
 
 
 class LoggerQC:
+    """
+    Class dedicated to logging errors, warnings, info, and creating
+    reports for the netCDF quality control library
+
+    Attributes:
+    - reports: list of created reports
+    - errors: list of logged errors for the report being created
+    - warnings: list of logged warnings for the report being created
+    - info: list of messages for the report being created
+
+    Methods:
+    - add_error: method to add an error
+    - add_warning: method to add info
+    - add_info: method to add a message
+    - create_report: method to create a report
+    - get_latest_report: method to get the latest report
+    - get_all_reports: method to get all reports
+    """
     def __init__(self):
-        self.reports: list[dict] = list()
-        self.errors = list()
-        self.warnings = list()
-        self.info = list()
+        """
+        Constructor for the logger object
+        """
+        self.reports: list[dict] = []
+        self.errors = []
+        self.warnings = []
+        self.info = []
 
     def add_error(self, error: str):
+        """
+        Method dedicated to adding an error to the report being made
+        :param error: error to be added
+        """
         self.errors.append(error)
 
     def add_warning(self, warning: str):
+        """
+        Method dedicated to adding a warning to the report being made
+        :param warning: warning to be added
+        """
         self.warnings.append(warning)
 
     def add_info(self, msg: str):
+        """
+        Method dedicated to adding a message to the report being made
+        :param msg: message to be added
+        """
         self.info.append(msg)
 
     def create_report(self):
+        """
+        Method dedicated to creating a report and adding it to the list of reports
+
+        - the method creates a report in the form of a dictionary, adds it to the
+        list of reports, and resets the objects attributes
+        """
         report_dict = {
             'report_date': date.today().strftime("%d-%m-%Y"),
             'report_time': datetime.now().strftime("%H:%M:%S"),
@@ -26,22 +68,22 @@ class LoggerQC:
             'info': self.info
         }
         self.reports.append(report_dict)
-        self.errors = list()
-        self.warnings = list()
-        self.info = list()
+        self.errors = []
+        self.warnings = []
+        self.info = []
 
     def get_latest_report(self) -> dict:
+        """
+        Method dedicated to getting the latest report made
+        :return: the latest report
+        """
         if len(self.reports) == 0:
             return {}
         return self.reports[len(self.reports) - 1]
 
     def get_all_reports(self) -> list[dict]:
+        """
+        Method dedicated to getting all reports made
+        :return: all reports
+        """
         return self.reports
-
-
-if __name__ == '__main__':
-    logger = LoggerQC()
-    logger.add_error("maika ti e mazh")
-    logger.create_report()
-    report = logger.get_latest_report()
-    print(report)
