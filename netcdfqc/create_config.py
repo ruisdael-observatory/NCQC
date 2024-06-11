914631a0375a11ff45d0423d52807a22a5bd8194
Jesse Vleeschdraager <J.C.Vleeschdraager@student.tudelft.nl>
2024-06-11 20:30:36 +0200

    Generalize implementation for telegram_fields

--- netcdfqc/create_config.py
+++ netcdfqc/create_config.py
@@ -1,6 +1,10 @@
 """
 Module for creating the base for a config file that can be used for the QualityControl object
 by parsing other more general config files.
+
+Functions:
+- create_config_dict_from_conf: Parses the given config file to create a dictionary which can be used for QC
+- create_config_dict_from_dict: Parses the given dictionary to create a dictionary which can be used for QC
 """
 
 from pathlib import Path
@@ -10,41 +14,44 @@ import yaml
 from netcdfqc.QCnetCDF import yaml2dict
 
 def create_config_dict_from_conf(path: Path,
-                                 dimensions_names: str = 'dimensions',
-                                 variables_names: str = 'variables',
-                                 global_attributes_names: str = 'global_attributes',
+                                 dimensions_name: str = 'dimensions',
+                                 variables_name: str = 'variables',
+                                 global_attributes_name: str = 'global_attributes',
+                                 field_names: List[str] = ['telegram_fields', 'var_attrs', 'standard_name'],
                                  min_file_size: int = 0) -> Dict:
     """
     Creates a config file for QC by parsing the given config file.
 
     :param path: path to the config file to parse
-    :param dimension_names: name or list of names of the groups containing the dimensions
-    :param variables_names: name or list of names of the groups containing the variables
-    :param global_attributes_names: name or list of names of the groups containing the global attributes
+    :param dimension_names: name of the groups containing the dimensions
+    :param variables_name: name of the groups containing the variables
+    :param global_attributes_name: name of the groups containing the global attributes
     :param path: the minimal file size that the netCDF files should have
     :return: a dictionary which contains the structure for specifying QC checks
     """
-    new_checks_dict = yaml2dict(path_qc_checks_file)
-    return create_config_file_from_dict(input_dict=new_checks_dict,
-                                        dimensions_names=dimensions_names,
-                                        variables_names=variables_names,
-                                        global_attributes_names=global_attributes_names,
+    new_checks_dict = yaml2dict(path)
+    return create_config_dict_from_dict(input_dict=new_checks_dict,
+                                        dimensions_name=dimensions_name,
+                                        variables_name=variables_name,
+                                        global_attributes_name=global_attributes_name,
+                                        field_names=field_names,
                                         min_file_size=min_file_size)
 
 def create_config_dict_from_dict(input_dict: Dict,
-                                 dimensions_names: str = 'dimensions',
-                                 variables_names: str = 'variables',
-                                 global_attributes_names: str = 'global_attributes',
-                                 telegram_names: str = 'telegram_fields',
+                                 dimensions_name: str = 'dimensions',
+                                 variables_name: str = 'variables',
+                                 global_attributes_name: str = 'global_attributes',
+                                 field_names: List[str] = ['telegram_fields', 'var_attrs', 'standard_name'],
                                  min_file_size: int = 0) -> Dict:
     """
     Creates a config file for QC by parsing the given dictionary.
 
     :param dict: the dictionary to parse
-    :param dimension_names: name or list of names of the groups containing the dimensions
-    :param variables_names: name or list of names of the groups containing the variables
-    :param global_attributes_names: name or list of names of the groups containing the global attributes
-    :param path: the minimal file size that the netCDF files should have
+    :param dimension_names: name of the groups containing the dimensions
+    :param variables_name: name of the groups containing the variables
+    :param global_attributes_name: name of the groups containing the global attributes
+    :param field_names: a list of names to follow to get the names of field variables
+    :param min_file_size: the minimal file size that the netCDF files should have
     :return: a dictionary which contains the structure for specifying QC checks
     """
     qc_dict = {
@@ -54,26 +61,44 @@ def create_config_dict_from_dict(input_dict: Dict,
         'min_file_size': min_file_size
     }
 
-    if dimensions_names in list(input_dict.keys()):
-        qc_dict['dimensions'].update(input_dict[dimensions_names])
+    # Add the dimensions to the dimensions group of the dictionary
+    if dimensions_name in list(input_dict.keys()):
+        qc_dict['dimensions'].update(input_dict[dimensions_name])
 
-    if variables_names in list(input_dict.keys()):
-        qc_dict['variables'].update(input_dict[variables_names])
+    # Add the variables to the variables group of the dictionary
+    if variables_name in list(input_dict.keys()):
+        qc_dict['variables'].update(input_dict[variables_name])
 
-    if telegram_names in list(input_dict.keys()):
-        for field in input_dict[telegram_names]:
-            field_name = input_dict[telegram_names][field]['var_attrs']['standard_name']
-            qc_dict['variables'].update({field_name: {}})
+    # Add the (telegram_)fields to the variables group of the dictionary
+    if field_names[0] in list(input_dict.keys()):
+        # If there is just one item in the list, add all fields within the group which that string defines
+        if len(field_names) == 1:
+            qc_dict['variables'].update(input_dict[field_names[0]])
+        # Individually go through the fields of that group
+        else:
+            for field in input_dict[field_names[0]]:
+                field_name = input_dict[field_names[0]][field]
 
-    if global_attributes_names in list(input_dict.keys()):
-        qc_dict['global_attributes'].update(input_dict[global_attributes_names])
+                i = 1
+                # Use the remaining strings in the list as a "path" to get to the name
+                while i < len(field_names):
+                    field_name = field_name[field_names[i]]
+                    i += 1
+                    print(len(dimensions_name))
 
+                qc_dict['variables'].update({field_name: {}})
 
+    # Add the global attributes to the global attributes group of the dictionary
+    if global_attributes_name in list(input_dict.keys()):
+        qc_dict['global_attributes'].update(input_dict[global_attributes_name])
+
+    # Give every dimension the setup for potentially applicable checks with "TO DO" for values
     for dim in qc_dict['dimensions']:
         qc_dict['dimensions'][dim] = {
             'does_it_exist_check': 'TODO'
         }
 
+    # Give every variable the setup for potentially applicable checks with "TO DO" for values
     for var in qc_dict['variables']:
         qc_dict['variables'][var] = {
             'does_it_exist_check': 'TODO',
@@ -84,15 +109,12 @@ def create_config_dict_from_dict(input_dict: Dict,
                 'upper_bound': 'TODO'
             }
         }
-        
-    print(qc_dict['variables'])
 
+    # Give every global attribute the setup for potentially applicable checks with "TO DO" for values
     for attr in qc_dict['global_attributes']:
         qc_dict['global_attributes'][attr] = {
             'does_it_exist_check': 'TODO',
             'is_it_empty_check': 'TODO'
         }
-
-    
 
     return qc_dict
--- tests/test_create_config.py
+++ tests/test_create_config.py
@@ -2,12 +2,14 @@
 Module for testing the automatic creation of config files for specifying checks.
 """
 import unittest
+from pathlib import Path
 from unittest.mock import patch, Mock
 
-from netcdfqc.create_config import create_config_dict_from_dict
+from netcdfqc.create_config import create_config_dict_from_conf, create_config_dict_from_dict
 
-def test_create_dict_success():
-    test_dict = {
+@patch('netcdfqc.create_config.yaml2dict')
+def test_replace_qc_checks_conf(mock_yaml2dict):
+    mock_yaml2dict.return_value = {
         'dimensions': {
             'dim1': 'value1',
             'dim2': 'value2'
@@ -89,9 +91,167 @@ def test_create_dict_success():
         'min_file_size': 0
     }
 
-    print(expected_dict['variables'])
+    qc_dict = create_config_dict_from_conf(path=Path('path'))
 
-    qc_dict = create_config_dict_from_dict(input_dict=test_dict)
+    assert qc_dict == expected_dict
 
+def test_create_from_dict_with_arguments():
+    test_dict = {
+        'dims': {
+            'dim1': 'value1',
+            'dim2': 'value2'
+        },
+        'vars': {
+            'var1': 'value1',
+            'var2': 'value2'
+        },
+        'global_attrs': {
+            'glattr1': 'value1'
+        },
+        'fields': {
+            '01': {
+                'dimensions': 'time',
+                'attrs': {
+                    'long_name': 'telegram field one',
+                    'short_name': 'field_1'
+                }
+            },
+            '02': {
+                'dimensions': 'time',
+                'attrs': {
+                    'long_name': 'telegram field two',
+                    'short_name': 'field_2'
+                }
+            }
+        }
+    }
+
+    expected_dict = {
+        'dimensions': {
+            'dim1': {'does_it_exist_check': 'TODO'},
+            'dim2': {'does_it_exist_check': 'TODO'}
+        },
+        'variables': {
+            'var1': {
+                'does_it_exist_check': 'TODO',
+                'is_it_empty_check': 'TODO',
+                'is_data_within_boundaries_check': {
+                    'perform_check': 'TODO',
+                    'lower_bound': 'TODO',
+                    'upper_bound': 'TODO'
+                }
+            },
+            'var2': {
+                'does_it_exist_check': 'TODO',
+                'is_it_empty_check': 'TODO',
+                'is_data_within_boundaries_check': {
+                    'perform_check': 'TODO',
+                    'lower_bound': 'TODO',
+                    'upper_bound': 'TODO'
+                }
+            },
+            'field_1': {
+                'does_it_exist_check': 'TODO',
+                'is_it_empty_check': 'TODO',
+                'is_data_within_boundaries_check': {
+                    'perform_check': 'TODO',
+                    'lower_bound': 'TODO',
+                    'upper_bound': 'TODO'
+                }
+            },
+            'field_2': {
+                'does_it_exist_check': 'TODO',
+                'is_it_empty_check': 'TODO',
+                'is_data_within_boundaries_check': {
+                    'perform_check': 'TODO',
+                    'lower_bound': 'TODO',
+                    'upper_bound': 'TODO'
+                }
+            }
+        },
+        'global_attributes': {
+            'glattr1': {
+                'does_it_exist_check': 'TODO',
+                'is_it_empty_check': 'TODO'
+            }
+        },
+        'min_file_size': 1024
+    }
+
+    qc_dict = create_config_dict_from_dict(input_dict=test_dict,
+                                           dimensions_name='dims',
+                                           variables_name='vars',
+                                           global_attributes_name='global_attrs',
+                                           field_names=['fields', 'attrs', 'short_name'],
+                                           min_file_size=1024)
+
+    assert qc_dict == expected_dict
+
+def test_create_from_dict_list_one_item():
+    test_dict = {
+        'dimensions': {
+            'dim1': 'value1',
+            'dim2': 'value2'
+        },
+        'variables': {
+            'var1': 'value1',
+            'var2': 'value2'
+        },
+        'global_attributes': {
+            'glattr1': 'value1'
+        },
+        'telegram_fields': {
+            '01': {
+                'dimensions': 'time',
+                'var_attrs': {
+                    'long_name': 'telegram field one',
+                    'standard_name': 'field_1'
+                }
+            },
+            '02': {
+                'dimensions': 'time',
+                'var_attrs': {
+                    'long_name': 'telegram field two',
+                    'standard_name': 'field_2'
+                }
+            }
+        }
+    }
+
+    expected_dict = {
+        'dimensions': {
+            'dim1': {'does_it_exist_check': 'TODO'},
+            'dim2': {'does_it_exist_check': 'TODO'}
+        },
+        'variables': {
+            'var1': {
+                'does_it_exist_check': 'TODO',
+                'is_it_empty_check': 'TODO',
+                'is_data_within_boundaries_check': {
+                    'perform_check': 'TODO',
+                    'lower_bound': 'TODO',
+                    'upper_bound': 'TODO'
+                }
+            },
+            'var2': {
+                'does_it_exist_check': 'TODO',
+                'is_it_empty_check': 'TODO',
+                'is_data_within_boundaries_check': {
+                    'perform_check': 'TODO',
+                    'lower_bound': 'TODO',
+                    'upper_bound': 'TODO'
+                }
+            }
+        },
+        'global_attributes': {
+            'glattr1': {
+                'does_it_exist_check': 'TODO',
+                'is_it_empty_check': 'TODO'
+            }
+        },
+        'min_file_size': 0
+    }
+
+    qc_dict = create_config_dict_from_dict(input_dict=test_dict, variables_name="none", field_names=["variables"])
 
     assert qc_dict == expected_dict
