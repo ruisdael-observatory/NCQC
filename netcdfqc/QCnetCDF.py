01b1d1c4e0c26344357a4347712baa06fd0c58e5
Vasil <v.chirov@student.tudelft.nl>
2024-06-14 17:39:07 +0200

    more documentation

--- netcdfqc/QCnetCDF.py
+++ netcdfqc/QCnetCDF.py
@@ -343,6 +343,11 @@ class QualityControl:
     def file_size_check(self):
         """
         Method to perform file size checks on the loaded netCDF file
+
+        - logs an error if there is no netCDF file loaded
+        - logs an error if the file size is out of the specified bounds
+        - logs an info message stating whether the check is successful or not
+
         :return: self
         """
         if self.nc is None:
