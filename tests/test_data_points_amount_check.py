b3eb6d984f5a824fdfe22b6d774d542c30117d5d
Vasil <v.chirov@student.tudelft.nl>
2024-06-16 18:54:42 +0200

    added documentation for the tests

--- tests/test_data_points_amount_check.py
+++ tests/test_data_points_amount_check.py
@@ -1,3 +1,6 @@
+"""
+Test module for the data_points_amount_check from QCnetCDF.py
+"""
 import os
 import unittest
 from pathlib import Path
@@ -11,8 +14,24 @@ nc_path = data_dir / 'test_data_points_amount.nc'
 
 
 class TestDataPointsAmountCheck(unittest.TestCase):
+    """
+    Class dedicated to testing the data_points_amount_check method from QCnetCDF.py
+
+    Methods:
+    - test_data_points_amount_check_success: Testing the data_points_amount_check method from
+     QCnetCDF.py with expected success
+    - test_data_points_amount_check_fail: Testing the data_points_amount_check method from QCnetCDF.py
+     with expected failure for one of the checks
+    - test_data_points_amount_check_no_such_var: Testing the data_points_amount_check method from
+     QCnetCDF.py with an expected warning for the variable specified not existing
+    - test_data_points_amount_check_no_nc: Testing the data_points_amount_check method from QCnetCDF.py
+     with an expected error for a netCDF file not being loaded
+    """
     @pytest.mark.usefixtures("create_nc_data_points_amount_check")
     def test_data_points_amount_check_success(self):
+        """
+        Testing the data_points_amount_check method from QCnetCDF.py with expected success
+        """
         qc_obj = QualityControl()
         qc_obj.load_netcdf(nc_path)
         qc_obj.add_qc_checks_dict({
@@ -42,6 +61,10 @@ class TestDataPointsAmountCheck(unittest.TestCase):
 
     @pytest.mark.usefixtures("create_nc_data_points_amount_check")
     def test_data_points_amount_check_fail(self):
+        """
+        Testing the data_points_amount_check method from QCnetCDF.py
+        with expected failure for one of the checks
+        """
         qc_obj = QualityControl()
         qc_obj.load_netcdf(nc_path)
         qc_obj.add_qc_checks_dict({
@@ -66,13 +89,17 @@ class TestDataPointsAmountCheck(unittest.TestCase):
         qc_obj.data_points_amount_check()
         assert qc_obj.logger.info == ["data points amount check for variable 'var_1d': SUCCESS",
                                       "data points amount check for variable 'var_2d': FAIL"]
-        assert qc_obj.logger.errors == [f"data points amount check error: number of data points (200)"
-                                        f" for variable 'var_2d' is below the specified threshold (201)"]
+        assert qc_obj.logger.errors == ["data points amount check error: number of data points (200)"
+                                        " for variable 'var_2d' is below the specified threshold (201)"]
         if os.path.exists(nc_path):
             os.remove(nc_path)
 
     @pytest.mark.usefixtures("create_nc_data_points_amount_check")
     def test_data_points_amount_check_no_such_var(self):
+        """
+        Testing the data_points_amount_check method from
+        QCnetCDF.py with an expected warning for the variable specified not existing
+        """
         qc_obj = QualityControl()
         qc_obj.load_netcdf(nc_path)
         qc_obj.add_qc_checks_dict({
@@ -96,6 +123,10 @@ class TestDataPointsAmountCheck(unittest.TestCase):
             os.remove(nc_path)
 
     def test_data_points_amount_check_no_nc(self):
+        """
+        Testing the data_points_amount_check method from QCnetCDF.py
+        with an expected error for a netCDF file not being loaded
+        """
         qc_obj = QualityControl()
         qc_obj.add_qc_checks_dict({
             'dimensions': {}, 'variables': {}, 'global attributes': {}, 'file size': {}
