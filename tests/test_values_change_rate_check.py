f9e465dccf0937a592e8d3d624727188e9766803
Ella Milinovic <e.milinovic@student.tudelft.nl>
2024-06-16 02:44:13 +0200

    Split up test.

--- tests/test_values_change_rate_check.py
+++ tests/test_values_change_rate_check.py
@@ -3,7 +3,8 @@ Module for testing the functionality of the value_change_rate_check method
 
 Functions:
 - test_value_change_rate_check_no_nc: Test for value change rate check when no netCDF file is loaded
-- test_change_rate: Test checking for the change rate of variables
+- test_change_rate_success: Test for when change rate of variables is acceptable
+- test_change_rate_fail: Test for when change rate of variables is not acceptable
 - test_change_rate_var_not_in_file: Test checking for the change rate of variables when
   variable is not in file.
 - test_change_rate_omitted_var: Tests checking for the change rate of variables when variable
@@ -18,13 +19,24 @@ from netcdfqc.QCnetCDF import QualityControl
 
 data_dir = Path(__file__).parent.parent / 'sample_data'
 
-change_rate_check_test_dict = {
+change_rate_check_test_dict_success = {
     'dimensions': {
     },
     'variables': {
         'test_pass': {
             'do_values_change_at_acceptable_rate_check': {'perform_check': True, 'acceptable_difference': 1}
         },
+    },
+    'global attributes': {
+    },
+    'file size': {
+    }
+}
+
+change_rate_check_test_dict_fail = {
+    'dimensions': {
+    },
+    'variables': {
         'test_fail': {
             'do_values_change_at_acceptable_rate_check': {'perform_check': True, 'acceptable_difference': 1}
         },
@@ -71,7 +83,7 @@ def test_value_change_rate_check_no_nc():
     Test for value change rate check when no netCDF file is loaded.
     """
     qc_obj = QualityControl()
-    qc_obj.add_qc_checks_dict(change_rate_check_test_dict)
+    qc_obj.add_qc_checks_dict(change_rate_check_test_dict_success)
     qc_obj.values_change_rate_check()
     assert not qc_obj.logger.info
     assert qc_obj.logger.errors == ['values change rate check error: no nc file loaded']
@@ -79,21 +91,41 @@ def test_value_change_rate_check_no_nc():
 
 
 @pytest.mark.usefixtures("create_nc_change_rate_check")
-def test_change_rate():
+def test_change_rate_success():
     """
-    Test checking for the change rate of variables.
+    Test for when change rate of variables is acceptable.
     """
     qc_obj = QualityControl()
 
     nc_path = data_dir / 'test_change_rate.nc'
     qc_obj.load_netcdf(nc_path)
 
-    qc_obj.add_qc_checks_dict(change_rate_check_test_dict)
+    qc_obj.add_qc_checks_dict(change_rate_check_test_dict_success)
 
     qc_obj.values_change_rate_check()
 
-    assert qc_obj.logger.info == ["value change rate check for variable 'test_pass': success",
-                                  "value change rate check for variable 'test_fail': fail"]
+    assert qc_obj.logger.info == ["value change rate check for variable 'test_pass': success"]
+    assert not qc_obj.logger.errors
+    assert not qc_obj.logger.warnings
+
+    if os.path.exists(nc_path):
+        os.remove(nc_path)
+
+@pytest.mark.usefixtures("create_nc_change_rate_check")
+def test_change_rate_fail():
+    """
+    Test for when change rate of variables is acceptable.
+    """
+    qc_obj = QualityControl()
+
+    nc_path = data_dir / 'test_change_rate.nc'
+    qc_obj.load_netcdf(nc_path)
+
+    qc_obj.add_qc_checks_dict(change_rate_check_test_dict_fail)
+
+    qc_obj.values_change_rate_check()
+
+    assert qc_obj.logger.info == ["value change rate check for variable 'test_fail': fail"]
     assert not qc_obj.logger.errors
     assert not qc_obj.logger.warnings
 
