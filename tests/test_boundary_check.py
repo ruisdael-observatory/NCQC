f887f7b842ca1fe85bda97e52f62c0a5cdd40af0
Vasil <v.chirov@student.tudelft.nl>
2024-06-17 17:19:15 +0200

    fic conficts

--- tests/test_boundary_check.py
+++ tests/test_boundary_check.py
@@ -8,14 +8,14 @@ Functions:
 - test_boundary_check_wrong_var_name: Test for the boundaries check when a variable to be
 checked is not in the loaded netCDF file
 - test_boundary_check_omit_a_var: Test for the boundaries check when a variable has to be omitted
-- test_boundary_check_multidim_var_success: Test for the boundaries check when a variable is multidimensional
-with expected success
-- test_boundary_check_multidim_var_fail: Test for the boundaries check when a variable is multidimensional
-with expected failure
 - test_boundary_check_property_based_success: Property based test for the boundaries check
     when all values are within the specified boundaries
 - test_boundary_check_property_based_fail: Property based test for the boundaries check
     when at least one value is outside of the specified boundaries
+- test_boundary_check_multidim_var_success: Test for the boundaries check when a variable is multidimensional
+with expected success
+- test_boundary_check_multidim_var_fail: Test for the boundaries check when a variable is multidimensional
+with expected failure
 """
 
 import os
@@ -53,24 +53,24 @@ boundary_check_test_dict = {
 }
 
 boundary_check_property_based_test_dict = {
-        'dimensions': {
-            'example_dimension_2': {'existence': False}
+    'dimensions': {
+        'example_dimension_2': {'existence': False}
+    },
+    'variables': {
+        'temperature': {
+            'existence': True,
+            'is_data_within_boundaries_check': {'perform_check': True, 'lower_bound': -10, 'upper_bound': 40}
         },
-        'variables': {
-            'temperature': {
-                'existence': True,
-                'is_data_within_boundaries_check': {'perform_check': True, 'lower_bound': -10, 'upper_bound': 40}
-            },
-        },
-        'global attributes': {
-            'existence': True, 'emptiness': True
-        },
-        'file size': {
-            'perform_check': True,
-            'lower_bound': 0,
-            'upper_bound': 1
-        }
+    },
+    'global attributes': {
+        'existence': True, 'emptiness': True
+    },
+    'file size': {
+        'perform_check': True,
+        'lower_bound': 0,
+        'upper_bound': 1
     }
+}
 
 def test_boundary_check_no_nc():
     """
@@ -232,6 +232,61 @@ def test_boundary_check_omit_a_var():
     if os.path.exists(nc_path):
         os.remove(nc_path)
 
+@given(data=st.lists(st.integers(min_value=-10, max_value=40), max_size=100))
+def test_boundary_check_property_based_success(data):
+    """
+    Property based test for the boundaries check when all values are within the specified boundaries
+    :param data: all possible lists of integers where all values are inside of the range [-10, 40]
+    """
+    create_nc_boundary_check_property_based(data=data)
+
+    qc_obj = QualityControl()
+
+    nc_path = data_dir / 'test_boundary_property.nc'
+    qc_obj.load_netcdf(nc_path)
+
+    qc_obj.add_qc_checks_dict(boundary_check_property_based_test_dict)
+
+    qc_obj.boundary_check()
+
+    assert qc_obj.logger.info == ['boundary check for variable \'temperature\': success']
+    assert not qc_obj.logger.errors
+    assert not qc_obj.logger.warnings
+
+    if os.path.exists(nc_path):
+        os.remove(nc_path)
+
+@given(data=st.lists(st.integers(), max_size=100)
+       .filter(lambda lst: any(x < -10 or x > 40 for x in lst)))
+def test_boundary_check_property_based_fail(data):
+    """
+    Property based test for the boundaries check when at least one value is outside of the specified boundaries
+    :param data: all possible lists of integers where at least value is outside of the range [-10, 40]
+    """
+    create_nc_boundary_check_property_based(data=data)
+
+    qc_obj = QualityControl()
+
+    nc_path = data_dir / 'test_boundary_property.nc'
+    qc_obj.load_netcdf(nc_path)
+
+    qc_obj.add_qc_checks_dict(boundary_check_property_based_test_dict)
+
+    qc_obj.boundary_check()
+
+    expected_errors = 0
+
+    for val in data:
+        if val < -10 or val > 40:
+            expected_errors += 1
+
+    assert qc_obj.logger.info == ['boundary check for variable \'temperature\': fail']
+    assert len(qc_obj.logger.errors) == expected_errors
+    assert not qc_obj.logger.warnings
+
+    if os.path.exists(nc_path):
+        os.remove(nc_path)
+
 @pytest.mark.usefixtures("create_nc_boundary_check_multidim_var")
 def test_boundary_check_multidim_var_success():
     """
@@ -267,6 +322,7 @@ def test_boundary_check_multidim_var_success():
     if os.path.exists(nc_path):
         os.remove(nc_path)
 
+
 @pytest.mark.usefixtures("create_nc_boundary_check_multidim_var")
 def test_boundary_check_multidim_var_fail():
     """
@@ -299,60 +355,6 @@ def test_boundary_check_multidim_var_fail():
     assert not qc_obj.logger.warnings
     assert qc_obj.logger.errors == ["boundary check error: '1.0099999904632568' out of"
                                     " bounds for variable 'var_2d' with bounds [0,1]"]
-    if os.path.exists(nc_path):
-        os.remove(nc_path)
-
-@given(data=st.lists(st.integers(), max_size=100)
-       .filter(lambda lst: any(x < -10 or x > 40 for x in lst)))
-def test_boundary_check_property_based_fail(data):
-    """
-    Property based test for the boundaries check when at least one value is outside of the specified boundaries
-    :param data: all possible lists of integers where at least value is outside of the range [-10, 40]
-    """
-    create_nc_boundary_check_property_based(data=data)
-
-    qc_obj = QualityControl()
-
-    nc_path = data_dir / 'test_boundary_property.nc'
-    qc_obj.load_netcdf(nc_path)
-
-    qc_obj.add_qc_checks_dict(boundary_check_property_based_test_dict)
-
-    qc_obj.boundary_check()
-
-    expected_errors = 0
-
-    for val in data:
-        if val < -10 or val > 40:
-            expected_errors += 1
-
-    assert qc_obj.logger.info == ['boundary check for variable \'temperature\': fail']
-    assert len(qc_obj.logger.errors) == expected_errors
-    assert not qc_obj.logger.warnings
-
-    if os.path.exists(nc_path):
-        os.remove(nc_path)
-
-@given(data=st.lists(st.integers(min_value=-10, max_value=40), max_size=100))
-def test_boundary_check_property_based_success(data):
-    """
-    Property based test for the boundaries check when all values are within the specified boundaries
-    :param data: all possible lists of integers where all values are inside of the range [-10, 40]
-    """
-    create_nc_boundary_check_property_based(data=data)
-
-    qc_obj = QualityControl()
-
-    nc_path = data_dir / 'test_boundary_property.nc'
-    qc_obj.load_netcdf(nc_path)
-
-    qc_obj.add_qc_checks_dict(boundary_check_property_based_test_dict)
-
-    qc_obj.boundary_check()
-
-    assert qc_obj.logger.info == ['boundary check for variable \'temperature\': success']
-    assert not qc_obj.logger.errors
-    assert not qc_obj.logger.warnings
 
     if os.path.exists(nc_path):
         os.remove(nc_path)
