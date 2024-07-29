00c311674b2504eec9d426f2201eb27912fdac2e
Andre Castro <a.i.castro@tudelft.nl>
2024-07-29 12:21:28 +0200

    pyproject.toml: build-system and project sections

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=find_packages(include=['ncqc']),
     version='0.1.0',
     description='Library to perform quality control on netCDF files.',
-    author='Noky Soekarman, Jesse Vleeschdraager, Mels Lutgerink, Ella Milinović, Vasil Chirov',
+    author='Noky Soekarman, Jesse Vleeschdraager, Mels Lutgerink, Ella Milinović, Vasil Chirov, Andre Castro, Marc Schleiss',
     install_requires=[
         'PyYAML',
         'pytest',
